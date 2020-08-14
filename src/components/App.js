5e4cf6ba47bc68e27c160306c48088abdb443535
Ze Dang <4649tz@gmail.com>
2020-08-14 13:52:32 +0800

    [zedang] feat: initialize project.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './modules/Navbar';
+
+class App extends Component {
+  render() {
+    return <React.Fragment>React App</React.Fragment>;
+  }
+}
+
+export default App;
